e351523e1bd62e2f322577bb5669b7b280e84554
DESKTOP-6QAGIT5\Theja <nitinalexvaidyan@gmail.com>
2026-01-08 16:44:07 +0530

    gateway microservice development

---
+++ python/src/gateway/server.py
@@ -0,0 +1,54 @@
+import os, gridfs, pika, json
+from flask import Flask, request, send_file
+from flask_pymongo import PyMongo
+from auth import validate
+from auth_svc import access
+from storage import util
+from bson.objectid import ObjectId
+
+server = Flask(__name__)
+mongo_video = PyMongo(server, uri="mongodb://host.minikube.internal:27017/videos")
+mongo_mp3 = PyMongo(server, uri="mongodb://host.minikube.internal:27017/mp3s")
+fs_videos = gridfs.GridFS(mongo_video.db)
+fs_mp3s = gridfs.GridFS(mongo_mp3.db)
+
+connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
+channel = connection.channel()
+
+@server.route("/healthcheck", methods=["GET"])
+def healthcheck():
+    return "Server is up and running", 200
+
+@server.route("/login", methods=["POST"])
+def login():
+    token, err = access.login(request)
+    if not err:
+        return token
+    else:
+        return err[0], err[1]
+
+
+@server.route("/upload", methods=['POST'])
+def upload():
+    access, err = validate.token(request)
+    access = json.loads(access)
+    if access["admin"]:
+        if len(request.files)==1:
+            return "excatly 1 file required", 400
+        
+        for _, f in request.files.items():
+            err = util.upload(f, fs, channel, access)
+            if err:
+                return err
+        return "success", 200
+    else:
+        return "not authorized", 401
+
+
+@server.route("/download", methods=["POST"])
+def download():
+    ...
+
+
+if __name__=="__main__":
+    server.run(host="0.0.0.0", port=8080)
